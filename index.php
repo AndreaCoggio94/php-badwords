5a11164f1e832538c60eb06bab603635e77530dc
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-25 16:28:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$paragraph = "";
+$censure = "";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <form action="./result.php">
+        <input type="text">
+        <input type="text">
+        <input type="submit" value="">
+    </form>
+</body>
+</html>
+
+
