9da31d4e2879c6b7d7c2d62acc6946205ce39bde
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-25 17:02:47 +0200

    added form and result php page

--- result.php
+++ result.php
@@ -2,12 +2,29 @@
 
 $paragraph = $_GET ["paragraph"] ;
 $censure = $_GET ["censure"] ;
-echo $_GET ["paragraph"] ;
+$solution = str_replace($censure, "***" , $paragraph) ;
 
-echo $_GET ["censure"] ;
+
+
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Il paragrafo intero è :</p>
+    <h1>
+   <?php echo $paragraph ?>
+    </h1>
+    <p>Mentre con la parola censurata " <?php echo $censure ?> " è :</p>
+    <h1>
+        <?php echo $solution ?>
+    </h1>
+</body>
+</html>
 
-<?php
-?>
